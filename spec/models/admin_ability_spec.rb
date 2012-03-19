92a3fe8bd2e93f2ea93a3d97839c8905480a92a9
Matt Topper <matt@matttopper.com>
2012-03-19 11:57:50 -0400

    I think we are working, and the admin Specs are in place.

--- spec/models/admin_ability_spec.rb
+++ spec/models/admin_ability_spec.rb
@@ -10,29 +10,28 @@ describe AdminAbility do
     ability.should be_able_to(:read, google)
   end
 
-  it 'lets a superuser manage all companies' do
-    bookface = Company.create(:name => 'Bookface' )
-    jesus = User.create(:company_id => nil, :password => 'passpass', :email=>'jesus@heaven.com', :superadmin => 'true', :role => 'superuser')
-    jesus_rights = AdminAbility.new(jesus)
-    jesus_rights.should be_able_to(:destroy, bookface)    
-  end
-  
-  it 'prevents a regular user from managing companies he does not belong to' do
-    twitter = Company.find_or_create_by_name(:name => 'Twitter' )
-    # we should only have one company in our test DB with transaction rollbacks so to create a user
-    # that doesn't belong to any company, we'll just increment the id by one
-    normal_guy = User.find_or_create_by_email(:company_id => twitter.id + 1, :password => 'passpass', :email=>'joe@gmail.com', :role => 'user')
-    twitter.id.should_not == normal_guy.company_id
-    
-    non_admin = AdminAbility.new(normal_guy)
-    non_admin.should_not be_able_to(:destroy, twitter)
-  end
-  
-  it 'lets a normal user read companies like through activeadmin' do
+  it 'lets a regular user read Contactors like through activeadmin' do
     livingsocial = Company.find_or_create_by_name(:name => 'Living Social' )
     normal_guy = User.find_or_create_by_email(:company_id => nil, :password => 'passpass', :email=>'joe@gmail.com', :role => 'user')
     non_admin = AdminAbility.new(normal_guy)
     non_admin.should be_able_to(:read, livingsocial)
   end
 
+  it 'prevents a regular user from managing companies he does not belong to' do
+    twitter = Company.find_or_create_by_name(:name => 'Twitter' )
+    # we should only have one company in our test DB with transaction rollbacks so to create a user
+    # that doesn't belong to any company, we'll just increment the id by one
+    normal_guy = User.find_or_create_by_email(:company_id => twitter.id + 1, :password => 'passpass', :email=>'joe@gmail.com', :role => 'user')
+    twitter.id.should_not == normal_guy.company_id
+    non_admin = AdminAbility.new(normal_guy)
+    non_admin.should_not be_able_to(:destroy, twitter)
+  end
+
+  it 'lets a superuser manage all companies' do
+    bookface = Company.create(:name => 'Bookface' )
+    frank = User.create(:company_id => nil, :password => 'passpass', :email=>'frank@blah.com', :superadmin => 'true', :role => 'superuser')
+    frank_rights = AdminAbility.new(frank)
+    frank_rights.should be_able_to(:destroy, bookface)    
+  end
+  
 end
